42572f11dbd4cefa023668ea5d3dc5987e202474
mdemo <mengdesen09@qq.com>
2014-11-13 16:14:54 +0800

    code format

--- index.js
+++ index.js
@@ -8,25 +8,25 @@ var fs = require('fs');
 var dirname = path.dirname(require.main.filename);
 
 
-var bootstrap = function(options){
+var bootstrap = function (options) {
   var files = [];
   var routers = [];
   options = options || {};
   var directory = options.directory || 'controllers';
-  var directory = path.join(dirname , directory);
-  if(!fs.existsSync(directory)){
+  var directory = path.join(dirname, directory);
+  if (!fs.existsSync(directory)) {
     var err = 'directory ' + options.directory + " doesn't exist";
     throw new Error(err);
   }
-  rd.eachFileFilterSync(directory,/\.js$/,function(file){
+  rd.eachFileFilterSync(directory, /\.js$/, function (file) {
     var dirname = path.dirname(file);
     var basename = path.basename(file);
-    files.push({dirname:dirname,basename:basename});
+    files.push({dirname: dirname, basename: basename});
   });
-  files.forEach(function(file){
-    if(file && file.basename != 'index.js'){
-      file.basename = file.basename.substring(0, file.basename.length-3)
-      file.dirname = path.join(file.dirname,file.basename);
+  files.forEach(function (file) {
+    if (file && file.basename != 'index.js') {
+      file.basename = file.basename.substring(0, file.basename.length - 3)
+      file.dirname = path.join(file.dirname, file.basename);
     }
     var route = '/' + strip(file.dirname, directory);
     var router = require(file.dirname)(new Router());
